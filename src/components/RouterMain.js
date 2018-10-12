1bacebf750d89c0d7e14adef9ddac96bfd087be9
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-10-12 09:09:35 -0500

    Append the route to display a blog

--- src/components/RouterMain.js
+++ src/components/RouterMain.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HomeView from '../views/Home';
 import NewBlog from '../views/NewBlog';
+import Blog from '../views/Blog';
 
 import { Switch, Route } from 'react-router-dom';
 
@@ -9,6 +10,7 @@ const RouterMain = () => (
         <Switch>
             <Route exact path={`${process.env.PUBLIC_URL}/`} component={HomeView}/>
             <Route path={`${process.env.PUBLIC_URL}/blog/new`} component={NewBlog} />
+            <Route path={`${process.env.PUBLIC_URL}/blog/:id`} component={Blog} />
         </Switch>
     </main>
 );
